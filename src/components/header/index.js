f8e44cc3250dea27988d11c452a9e4a7dfb1ff73
kimicalilu <valera2491997@gmail.com>
2022-12-22 12:18:18 +0100

    fixed task-lisk

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,20 +1,22 @@
 import headerTemplate from './index.hbs'
-
-import  './header.less';
+import _ from 'lodash';
+import './header.less';
 
 export { headerTemplate }
 
 
-window.addEventListener('scroll',(event) => {
-    if(window.scrollY !== 0){
+function scrollDebounce(e) {
+
+    if (window.scrollY !== 0) {
+        console.log('123')
         document.querySelector('.header').classList.add('active')
         document.querySelector('.header__img').classList.add('active')
     }
-    else if(window.scrollY == 0){
+    else if (window.scrollY == 0) {
         document.querySelector('.header').classList.remove('active')
         document.querySelector('.header__img').classList.remove('active')
     }
-});
+}
+const debounce = _.debounce(scrollDebounce, 50);
 
-
-
+window.addEventListener('scroll', debounce);
